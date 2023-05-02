cca9a620a33158222b9b46165d7df932cab4b3ec
Adam <897017+aSemy@users.noreply.github.com>
2023-05-03 00:01:04 +0200

    limit snapshot repo to snapshots

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,9 @@ plugins {
 repositories {
    mavenCentral()
    mavenLocal()
-   maven("https://oss.sonatype.org/content/repositories/snapshots/")
+   maven("https://oss.sonatype.org/content/repositories/snapshots/") {
+      mavenContent { snapshotsOnly() }
+   }
    google()
    gradlePluginPortal() // tvOS builds need to be able to fetch a kotlin gradle plugin
 }
